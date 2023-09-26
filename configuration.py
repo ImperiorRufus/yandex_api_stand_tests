e340eeec35299ff5f2cd60420adef18087900b0d
Rushan Akthyamov <ahtyamov1995@gmail.com>
2023-09-26 11:11:49 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = 'https://5ee30947-f9ab-47b3-a139-dbf31d3a68f0.serverhub.praktikum-services.ru'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = "/api/logs/main/"
+params = {"count": 20}
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
